fd9c2e0bed3af80e257fac0fb91919c2158b6786
大石头 <nnhy@vip.qq.com>
2022-08-10 13:26:19 +0800

    RPC远程过程调用，二进制封装，提供高吞吐低延迟的高性能RPC框架

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using NewLife.Log;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            XTrace.UseConsole();
+
+            try
+            {
+                //TestHyperLogLog();
+                Test1();
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteException(ex);
+            }
+
+            Console.WriteLine("OK!");
+            Console.ReadKey();
+        }
+
+        static void Test1()
+        {
+
+        }
+    }
+}
